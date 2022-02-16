f766e11c273d8f914eacded5760b5227b0f0e5f3
João Victor Rossi Rabello <Jvrossi10@gmail.com>
2022-02-15 23:03:08 -0300

    primeira lista com html

---
+++ aula5/aula5.js
@@ -0,0 +1,23 @@
+// lista ordenada (1,2,3 ou A,B,C ou I,II,III)
+<ol type = '1'>
+    <li></li>
+    <li></li>
+</ol>
+
+//lista não ordenada
+<ul type = 'disc'>
+    <li></li>
+</ul>
+
+// listas aninhadas (uma lista ordenada esta dentro de uma outra nao alinhada)
+<ul>
+    <li>
+        <ol>
+            Lembrete para viagem:
+            <li>Identidades</li>
+            <li>Passagens</li>
+        </ol>
+    </li>
+    <li>Ligar parar a Assist Card</li>
+    <li>Adaptador para celular</li>
+</ul> 
